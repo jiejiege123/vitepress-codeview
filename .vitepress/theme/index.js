ed64bee273cb53dcf9f64a0cc489dfc975bbb430
zzz <2395436684@qq.com>
2021-12-03 14:11:30 +0800

    feat: 删除多余文件

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,10 @@
+/*
+ * @Author: zzz
+ * @LastEditors: zzz
+ */
 import 'element-plus/dist/index.css'
 
-import DefaultTheme from './theme-element'
-
+import DefaultTheme from 'vitepress/theme'
 
 import VpDemo from '../vitepress/components/vp-demo.vue'
 
@@ -9,6 +12,7 @@ import Layout from './Layout.vue'
 
 export default {
   ...DefaultTheme,
+  // 取消引用，恢复系统默认文档目录
   Layout,
   enhanceApp({ app }) {
     app.component('VpDemo', VpDemo)
--- guide/todo.md
+++ guide/todo.md
@@ -10,7 +10,7 @@
 2. <label><input disabled name="Fruit" type="checkbox" >html 文件修改后，效果展示和代码显示都有问题。
 </label> 
 
-3. <label><input disabled name="Fruit" type="checkbox" >更改目录到右侧
+3. <label><input disabled name="Fruit" type="checkbox" checked>更改目录到右侧
 </label> 
 
 4. <label><input disabled name="Fruit" type="checkbox" >增加配置显示不同的文章目录位置</label> 
